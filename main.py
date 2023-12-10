e5803ffe942d11268a44abd8641385ddbeb7d6be
daniel-2-7 <75557992+daniel-2-7@users.noreply.github.com>
2023-12-10 15:59:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, url_for, request, flash
 from website import create_app
 
-app = create_app()
+
 
 # Running application
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
 
 
